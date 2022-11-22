f13365d460eb4dde9f5d5a0f9e0b9c3f161a81ed
Lester bon F Biono <lesterb@swplumb.com>
2022-11-22 16:11:35 +0800

    Added &&

--- change_file_permission.bash
+++ change_file_permission.bash
@@ -1,6 +1,6 @@
-sudo chmod 666 data/games_list.json
-sudo chmod 666 data/games.json
-sudo chmod 777 games_v2.php
-sudo chmod 755 *.php -r
-sudo chmod 755 data/*.php -r
-sudo chmod 755 images/* -r
+sudo chmod 666 data/games_list.json &&
+sudo chmod 666 data/games.json &&
+sudo chmod 777 games_v2.php &&
+sudo chmod 755 *.php -r &&
+sudo chmod 755 data/*.php -r &&
+sudo chmod 755 images/* -r &&
