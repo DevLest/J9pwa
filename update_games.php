d66f82f6aa315137d339be55e774547e93819df9
root <root@ip-172-31-34-113.ap-east-1.compute.internal>
2022-11-22 15:42:22 +0800

    server updates

--- update_games.php
+++ update_games.php
@@ -18,21 +18,25 @@
     $filedata = json_decode(removeBomUtf8(file_get_contents($file)), JSON_UNESCAPED_UNICODE );
     $newJson = [];
 
-    foreach ($filedata as $detail){
-        if ($detail['id'] == $_GET['id'] && $detail['name'] == $_GET['name']) {
-            if (isset($_GET['state'])) {
-                $detail['state'] = (int) json_decode($_GET['state']);
+    if (is_array($filedata)) {
+        foreach ($filedata as $detail){
+            if ($detail['id'] == $_GET['id']) {
+                if (isset($_GET['state'])) {
+                    $detail['state'] = (int) json_decode($_GET['state']);
+                }
+                if (isset($_GET['tag'])) {
+                    $detail['tag'] = explode(",",$_GET['tag']);
+                }
             }
-            if (isset($_GET['tag'])) {
-                $detail['tag'] = explode(",",$_GET['tag']);
-            }
+    
+            array_push($newJson, $detail);
         }
-
-        array_push($newJson, $detail);
+    
+        file_put_contents($file, json_encode($newJson));
+        return json_encode(['status'=>1,'info'=>json_encode($newJson)], JSON_UNESCAPED_UNICODE );
     }
-
-    file_put_contents($file, json_encode($newJson));
-    return json_encode(['status'=>1,'info'=>json_encode($newJson)], JSON_UNESCAPED_UNICODE );
+    
+    return json_encode(['status'=>0,'info'=>print_r($filedata)], JSON_UNESCAPED_UNICODE );
 
     function removeBomUtf8($s){
         if(substr($s,0,3)==chr(hexdec('EF')).chr(hexdec('BB')).chr(hexdec('BF'))){
