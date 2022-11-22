c6539eb3c2d51c4b51831bf364f3b8c9d3d128dc
Lester bon F Biono <lesterb@swplumb.com>
2022-11-22 16:16:39 +0800

    move change permission seq

--- change_file_permission.bash
+++ change_file_permission.bash
@@ -1,6 +1,6 @@
 sudo chmod 666 data/games_list.json &&
 sudo chmod 666 data/games.json &&
-sudo chmod 777 games_v2.php &&
 sudo chmod 755 -R *.php &&
 sudo chmod 755 -R data/*.php &&
-sudo chmod 755 -R images/*
+sudo chmod 755 -R images/* &&
+sudo chmod 777 games_v2.php
