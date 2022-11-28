90bcd186f1bd8861d9607929a30629e393df94df
Lester bon F Biono <lesterb@swplumb.com>
2022-11-28 17:07:49 +0800

    Added classes for cron

---
+++ class/pt_usd_jackpot.php
@@ -0,0 +1,49 @@
+<?php
+    header("Content-type: text/html; charset=utf-8");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials:true");
+
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
+    include_once ("../common/cache_file.class.php");
+    
+    $filedata = json_decode(removeBomUtf8(file_get_contents("../data/games.json")), JSON_UNESCAPED_UNICODE );
+    $game = [];
+    
+    foreach ($filedata as $detail)
+    {
+        if ( !$detail['state'] ) continue;
+        if ( $detail['platform'] != "PT" ) continue;
+        if ( ( $detail['jackpot_ticker'] != "" && $detail['platform'] == "PT" ) )
+        {
+            $id = explode(',', $detail['jackpot_ticker']);
+            $url = "https://tickers.playtech.com/js?info=1&casino=1club&currency=usd&game=".$id[0];
+            $json_data = explode(";",@file_get_contents($url));
+        
+            $json = substr(str_replace("\\", "", str_replace("xmlstring.jpxml = ", "", $json_data[0])), 1, -1);
+            
+            $xml = simplexml_load_string($json);
+            $json = json_encode($xml);
+            $array = json_decode($json,TRUE);
+            
+            if (!empty($array )) 
+            {
+                $game[$detail['id']] = isset($array['gamedata']['amount-list']['amount']) ? floatval($array['gamedata']['amount-list']['amount']) : 0;
+            }
+        }
+    }
+
+    $cachFile = new cache_file();
+    $cachFile->set("jackpot",$game,'','data','pt');
+    echo count($game);;
+    
+    function removeBomUtf8($s){
+        if(substr($s,0,3)==chr(hexdec('EF')).chr(hexdec('BB')).chr(hexdec('BF'))){
+            return substr($s,3);
+        }else{
+            return $s;
+        }
+    }
+?>
---
+++ class/sw_usd_jackpot.php
@@ -0,0 +1,56 @@
+<?php
+    header("Content-type: text/html; charset=utf-8");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials:true");
+
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
+    include_once ("../common/cache_file.class.php");
+    
+    $filedata = json_decode(removeBomUtf8(file_get_contents("../data/games.json")), JSON_UNESCAPED_UNICODE );
+    $game = [];
+    $skip = [
+        'sw_2pd'
+    ];
+    
+    foreach ($filedata as $detail)
+    {
+        if ( !$detail['state'] ) continue;
+        if ( $detail['platform'] != "SW" ) continue;
+        if ( ( $detail['jackpot_ticker'] != "" && $detail['platform'] == "SW" ) )
+        {
+            $id = explode(',', $detail['jackpot_ticker']);
+            $url = "https://jpn-ticker-eu-gcp-str.ss211208.com/v1/ticker?currency=USD&jackpotIds=".$id[0];
+            
+            $array = json_decode(@file_get_contents($url),TRUE);
+            
+            if (!empty($array )) 
+            {
+                $pools = (!in_array($skip, $detail['id'])) ? $array[0]['pools'] : 10000;
+                usort($pools, function($a, $b) 
+                { 
+                    if($a['amount']==$b['amount']) return 0;
+                    return $a['amount'] < $b['amount'] ? 1 : -1;
+                });
+
+                $pool = array_values($pools)[0];
+                print_r( $detail['tag'] ); echo " | ". $detail['id']."\n";
+                $game[$detail['id']] = $pool['amount'] > 0 ? $pool['amount'] : 0;
+            }
+        }
+    }
+
+    $cachFile = new cache_file();
+    $cachFile->set("jackpot_amount",$game,'','data','sw');
+    echo count($game);;
+    
+    function removeBomUtf8($s){
+        if(substr($s,0,3)==chr(hexdec('EF')).chr(hexdec('BB')).chr(hexdec('BF'))){
+            return substr($s,3);
+        }else{
+            return $s;
+        }
+    }
+?>
