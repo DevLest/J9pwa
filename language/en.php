ea14392e5c728f900060af93334ca2d155352aa2
Lester bon F Biono <lesterb@swplumb.com>
2022-11-29 13:35:52 +0800

    Added language file

---
+++ language/en.php
@@ -0,0 +1,25 @@
+<?php
+    return [
+        "request" => [
+            "auth_check" => "Verification failed",
+            "play_game" => [
+                "error_message_1" => "Invalid Parameters",
+            ],
+            "change_account_info" => [
+                "success_message_1" => "Information modified successfully",
+            ],
+            "send_reset_password" => [
+                "mail_success" => "The message has been sent",
+                "mail_error" => "Error in sending reset code",
+                "code_error" => "Error in sending reset code | Password code Ivanlid",
+                "smtp_catch" => "Error in sending reset code. Error: ",
+                "check_email" => "Email not registered in any guest account",
+            ],
+            "verify_reset_password" => [
+                "code_expired" => "Verification code expired! Please request a new code.",
+            ],
+
+
+        ]
+    ]
+?>
